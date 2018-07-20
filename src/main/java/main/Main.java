f509b3af22451139f34e5ee42b324fe6ef174961
GaoGian <447647833@qq.com>
2018-07-20 09:43:07 +0800

    项目搭建

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.io.IOException;
+
+/**
+ * Created by tingyun on 2018/7/20.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
