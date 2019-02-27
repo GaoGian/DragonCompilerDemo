1390af45920916024aed8539cde5cf5570c24832
GaoGian <447647833@qq.com>
2019-02-27 23:12:12 +0800

    增加可视化页面

--- src/test/java/SyntaxTest.java
+++ src/test/java/SyntaxTest.java
@@ -279,10 +279,22 @@ public class SyntaxTest {
 
         LexUtils.outputLL1SyntaxPredict(syntaxFirstMap, syntaxFollowMap, syntaxPredictMap);
 
-        System.out.println("-------------------------------LL(1)语法分析----------------------------------");
-
-        // TODO 需要将预测分析表的终结符替换成元字符，识别成类似Token的封装类
+        System.out.println("------------------------------解析字符串------------------------------------");
+        // TODO 文法中的终结符需要和 LexExpression 元字符进行匹配，然后在判断输入符是否是对应的LexExpression的符号（验证正则表达式是否匹配，已经TokenType是否一致）
+        // TODO 需要区分关键字和ID类别的终结符
+        /** 一般情况下词法分析是在文法分析过程中进行的，根据语义返回识别的符号，这里由于分开处理，所以需要进行额外的关联映射 **/
         List<Token> tokens = LexicalParser.parser("C:\\Users\\Gian\\Desktop\\Temp\\compilerCode.txt");
+        int line = 0;
+        for(Token token : tokens){
+            if(token.getLine() > line){
+                line = token.getLine();
+                System.out.println("");
+            }
+            System.out.print(token.toString());
+        }
+        System.out.println("");
+
+        System.out.println("-------------------------------LL(1)语法分析----------------------------------");
         SyntacticParser.syntaxParse(tokens, syntaxSymbols.get(0), syntaxPredictMap);
 
     }
