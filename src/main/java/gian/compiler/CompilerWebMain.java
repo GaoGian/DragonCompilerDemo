7baa24e72a15f263dc33c435056b85cca474e453
GaoGian <447647833@qq.com>
2019-04-15 16:29:02 +0800

    调整web访问方式

---
+++ src/main/java/gian/compiler/CompilerWebMain.java
@@ -0,0 +1,16 @@
+package gian.compiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by gaojian on 2019/4/15.
+ */
+@SpringBootApplication
+public class CompilerWebMain {
+
+    public static void main(String[] args){
+        SpringApplication.run(CompilerWebMain.class);
+    }
+
+}
