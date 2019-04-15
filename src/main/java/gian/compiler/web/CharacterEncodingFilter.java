1437959e21afe327e8ce86191608429891840e7c
GaoGian <447647833@qq.com>
2019-04-15 16:46:41 +0800

    解决乱码问题

---
+++ src/main/java/gian/compiler/web/CharacterEncodingFilter.java
@@ -0,0 +1,32 @@
+package gian.compiler.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by gaojian on 2019/4/15.
+ */
+@WebFilter(urlPatterns = "/*",filterName = "CharacterEncodingFilter")
+public class CharacterEncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        filterChain.doFilter(request , response);
+    }
+    @Override
+    public void destroy() {
+    }
+
+}
